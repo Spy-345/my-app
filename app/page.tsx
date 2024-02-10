3ef55fd8bcd3689f929d31f380cf68fb99deca28
Spy_345 <shivyewle705@gmail.com>
2024-02-10 22:25:16 +0530

    Second Commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Hello this file is Edited in the Neovim Editor!</div>
+      <div>This is updated code</div>
     </main>
   )
 }
